d65c0a4025463ecda7a5906cdaa6bea628f15410
Gokul <gokulmehta1777@gmail.com>
2023-07-27 12:00:54 +0530

    added variables learning material

---
+++ 05_Variables.php
@@ -0,0 +1,15 @@
+<?php
+// echo "This is tutorial 5";
+// Vaarables are Containers for storing information
+// Starts with a $
+$name = "Shubham";
+$namE = "Cappital wala Shubham";
+// Variables are case sensitive
+$income = 20000.5;
+
+echo "this guy is $name and his income is $income Rupees per month <br>";
+echo "$name is a good boy <br>";
+echo "$namE is a real gangsta <br>";
+
+
+?>

6179b71807e3a4a06870b77a5347675d4747530c
Gokul <gokulmehta1777@gmail.com>
2023-07-27 13:09:43 +0530

    If - Else - If statement learning stuff

---
+++ 10_if_else_conditionals.php
@@ -0,0 +1,40 @@
+<?php
+// $a = 665;
+// $b = 9;
+
+// if($a>78){
+// echo "a is greater than 78";
+// }else{
+//     echo "a is not greater than 78";
+// }
+
+$age = 24;
+
+//IF Else ladder
+// if ($age>18){
+//     echo "You can drink water with chai and alcohol";
+// }
+// elseif ($age>13){
+//     echo "You can drink chai only with water no alcohol for you";
+// }
+// else{
+//     echo "You can drink water only";
+// }
+
+if ($age>18){
+    echo "You can drink water with chai and alcohol <br>";
+}
+if ($age>13){
+    echo "You can drink chai only with water no alcohol for you <br>";
+}
+else{
+    echo "You can drink water only <br>";
+}
+
+
+
+echo "Done";
+
+//Quick quiz -Create an if else ladder using more than one else if
+
+?>

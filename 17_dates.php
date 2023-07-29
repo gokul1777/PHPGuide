7271ac5ce5b9fc04c9d42eea3204abb706b2b525
Gokul <gokulmehta1777@gmail.com>
2023-07-29 13:28:47 +0530

    Date Function Learning Stuff

---
+++ 17_dates.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Welcome to the world of dates <br>";
+$d =date("dS F Y g:i A");
+$today = date("F j, Y, g:i a");
+// $d =date("dS F Y");
+echo "Today's date is $d <br>";
+echo date('l \t\h\e jS'). "<br>";
+echo "$today <br>";
+
+$year = date("Y");
+echo "copyright $year All right reserved <br>";
+
+
+?>

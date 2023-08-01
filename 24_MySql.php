87b10043f964b72c8fc74919b87908ef08f961bd
Gokul <gokulmehta1777@gmail.com>
2023-08-01 10:18:32 +0530

    Connecting to MySQL Database from php - learning stuff

---
+++ 24_MySql.php
@@ -0,0 +1,22 @@
+<?php
+echo "Welcome to the stage where we are ready to get connected to a database <br>";
+// Ways to connect to a MySQL Database
+// 1. MySQLi extension 
+// 2. PDO (PHP Data Objects)
+
+//Connecting to a Database
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Die if connection was not successful
+if (!$conn){
+    die("Sorry we failed to connect:". mysqli_connect_error());
+}
+else{
+echo "Connecction was successful";
+}
+?>

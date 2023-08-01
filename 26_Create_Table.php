91d7a17bbb4a3f5a61439ae34359e5a1bcc76ab2
Gokul <gokulmehta1777@gmail.com>
2023-08-01 12:30:24 +0530

    Create a table in MySQL using PHP - Learnign stuff

---
+++ 26_Create_Table.php
@@ -0,0 +1,36 @@
+<?php
+
+//Connecting to a Database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "dbgokul";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Die if connection was not successful
+if (!$conn){
+    die("Sorry we failed to connect:". mysqli_connect_error());
+}
+else{
+echo "Connecction was successful <br>";
+}
+
+//Create a table in the db
+$sql = "CREATE TABLE `phptrip` ( `sno` INT(6) NOT NULL , `name` VARCHAR(12) NOT NULL , `dest` VARCHAR(6) NOT NULL ) ENGINE = InnoDB";
+$result = mysqli_query($conn, $sql);
+
+//Check for the table creation success
+
+if($result){
+    echo "The table was created successfully <br>";
+}
+else{
+    echo "The table was not created suuccesfully because of this error ---->" . mysqli_error($conn);
+}
+    
+
+
+
+?>

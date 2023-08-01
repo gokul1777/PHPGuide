5bc69bb28b2ae5d7a0e7963d2800d30ed2c679e6
Gokul <gokulmehta1777@gmail.com>
2023-08-01 11:07:51 +0530

    Creating a MySQL Database using PHP - learning stuff

---
+++ 25_MySql_Create_db.php
@@ -0,0 +1,37 @@
+<?php
+
+//Connecting to a Database
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Die if connection was not successful
+if (!$conn){
+    die("Sorry we failed to connect:". mysqli_connect_error());
+}
+else{
+echo "Connecction was successful <br>";
+}
+
+//Create a DB
+$sql = "CREATE DATABASE dbGokul6";
+$result = mysqli_query($conn, $sql);
+
+//Check for the database creation success
+
+if($result){
+    echo "The DB was created successfully <br>";
+}
+else{
+    echo "The DB was not created suuccesfully because of this error ---->" . mysqli_error($conn);
+}
+echo "The reesult is";
+echo var_dump($result);
+echo "<br>";
+
+
+
+?>

e62ac1e0c71ff9f8e8744064f504fe3e719ec11d
Gokul <gokulmehta1777@gmail.com>
2023-08-06 11:21:19 +0530

    fgetc() & fgets() in PHP learning stuff

---
+++ 36_files.php
@@ -0,0 +1,37 @@
+<?php
+
+$fptr = fopen("myfile.txt", "r");
+// echo fgets($fptr);
+// echo fgets($fptr);
+// echo fgets($fptr);
+// echo fgets($fptr);
+// echo var_dump(fgets($fptr));
+/*
+// Reading a file line by line
+while($a=fgets($fptr)){
+echo $a;
+}
+
+echo "End of the file has been reached";
+*/
+
+// echo fgetc($fptr);
+/*
+// Reading a file character by character
+while($a=fgetc($fptr)){
+echo $a;
+// break;
+}
+
+echo "End of the file has been reached";
+*/
+
+//Write a program which reads the content of the file until . has been encounterd
+while($a =fgetc($fptr)){
+    echo $a;
+    if($a == "."){
+        break;
+    }
+}
+fclose($fptr);
+?>

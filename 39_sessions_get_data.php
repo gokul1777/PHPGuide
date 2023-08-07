0b422c80ae91bd40d205e397da1f00917ac0c481
Gokul <gokulmehta1777@gmail.com>
2023-08-07 11:41:48 +0530

    Used to get data from Sessions - Learning Stuff

---
+++ 39_sessions_get_data.php
@@ -0,0 +1,16 @@
+<?php
+
+// Start the session and get the data
+session_start();
+if(isset($_SESSION['username'])){
+echo "Welcome " . $_SESSION['username'] ;
+echo "<br> Your favourite category is " . $_SESSION['favCat'] ;
+echo "<br>";
+}
+else{
+    echo "Please login to continue";
+}
+
+
+
+?>
